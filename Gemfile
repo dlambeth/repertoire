1b5bfb83a1e7f5b78cae7e9d5b67aa63afa27442
Dawn Lambeth <dawn@dawnlambeth.com>
2014-11-21 14:44:29 -0800

    finish user signup

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 
 gem 'rails',                   '4.2.0.beta4'
+#used for password migration
 gem 'bcrypt',                  '3.1.7'
 gem 'faker',                   '1.4.2'
 gem 'carrierwave',             '0.10.0'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+     #use byebug to enter debugging mode in the server
+    #debugger
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      #require the params hash to have a user attribute and 
+      #permit the user hash to have specified attributes.
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
