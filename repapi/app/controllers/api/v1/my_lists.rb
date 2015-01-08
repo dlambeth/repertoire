880468e3052d6dcd401d3ae1e361b0ed5018eb0c
Dawn Lambeth <dawn@dawnlambeth.com>
2015-01-08 11:39:33 -0800

    start to add support for devise users

--- repapi/app/controllers/api/v1/my_lists.rb
+++ repapi/app/controllers/api/v1/my_lists.rb
@@ -4,18 +4,32 @@ module API
       include API::V1::Defaults
 
       resource :my_lists do
-        desc "Return all lists"
+        desc "Return all lists for a given user"
         get "", root: :my_lists do
           MyList.all
         end
 
-        desc "Return a list"
+        desc "Return a list for a given user"
         params do
           requires :id, type: String, desc: "ID of the list"
         end
         get ":id", root: "my_list" do
           MyList.where(id: permitted_params[:id]).first!
         end
+
+        #desc "Create a list"
+        #params do
+        #  requires :title, type: String, desc: "Title of the list"
+        #end
+        #post do
+          #authenticate!
+          #???replace with current_user.lists.build(mylist_params)
+        #  MyList.create!({
+            #user: current_user,
+        #    user: 1
+        #    title: params[:title]
+        #  })
+        #end
       end
     end
   end
--- repapi/app/controllers/application_controller.rb
+++ repapi/app/controllers/application_controller.rb
@@ -2,12 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate_user_from_token!
 
+  #active admin authentication: so we can use the single user model for both 
+  #regular devise authentication and active admin support 
   def authenticate_active_admin_user!
     authenticate_user!
     unless current_user.superadmin?
       flash[:alert] = "Unauthorized Access!"
       redirect_to root_path
+    end
+  end
+
+  private
+ 
+    def authenticate_user_from_token!
+      authenticate_with_http_token do |token, options|
+      user_email = options[:user_email].presence
+      user       = user_email && User.find_by_email(user_email)
+ 
+      if user && Devise.secure_compare(user.authentication_token, token)
+        sign_in user, store: false
+      end
     end
   end
 end
