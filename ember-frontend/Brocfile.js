f9ff4b66d480e9cea3247e17bd3bc24fdf278c2c
Dawn Lambeth <dawn@dawnlambeth.com>
2015-01-09 12:37:22 -0800

    add support for fontawesome and bootstrap icons

--- ember-frontend/Brocfile.js
+++ ember-frontend/Brocfile.js
@@ -16,8 +16,19 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
-  destDir: 'assets'
+//app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+//app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+//  destDir: 'assets'
+//});
+app.import('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js');
+app.import('bower_components/fontawesome/fonts/fontawesome-webfont.ttf', {
+   destDir: 'assets'
 });
-module.exports = app.toTree();
+
+// Glyphicons
+var pickFiles = require('broccoli-static-compiler');
+var bootstrapFonts = pickFiles('bower_components/bootstrap-sass-official/assets/fonts/bootstrap', {
+     srcDir: '/',
+     destDir: '/fonts'
+});
+module.exports = app.toTree(bootstrapFonts);
